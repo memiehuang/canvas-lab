9cc594bc9b79348ff66292ba0002f2e7c6a89ad9
Memie Huang <memie.huang@gmail.com>
2015-11-19 15:30:10 -0800

    Draw Canvas Completed

--- js/app.js
+++ js/app.js
@@ -8,5 +8,34 @@
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
+    var canvas = document.getElementById("canvas");
+    var input = document.getElementById("line-color-inp");
+    var ctx = canvas.getContext('2d');
+    var mouseDown = false;
+
+    ctx.lineWidth = 30;
+
+    canvas.addEventListener('mousedown', function(mouse){
+        mouseDown = true;
+        ctx.beginPath();
+        ctx.moveTo(mouse.clientX - canvas.offsetLeft, mouse.clientY - canvas.offsetTop);
+    });
+
+    canvas.addEventListener('mousemove', function(mouse){
+        if(mouseDown){
+            ctx.lineTo(mouse.clientX - canvas.offsetLeft, mouse.clientY - canvas.offsetTop);
+            ctx.stroke();
+        }
+    });
+
+    canvas.addEventListener('mouseup', function(mouse){
+        ctx.lineTo(mouse.clientX - canvas.offsetLeft, mouse.clientY - canvas.offsetTop);
+        ctx.stroke();
+        mouseDown = false;
+    });
+
+    input.addEventListener('change', function(){
+       ctx.strokeStyle = document.getElementById('line-color-inp').value;
+    });
 
 }); //DOMContentLoaded
